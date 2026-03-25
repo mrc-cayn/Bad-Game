dd1c7fcd50648e2b4a1bcd992ef127614eb6cefa
mrc-cayn <mrc.cayn@gmail.com>
2026-03-25 15:44:10 +0530

    fixed level up bug

--- globals/global.gd
+++ globals/global.gd
@@ -4,8 +4,13 @@ var Current_level := 1
 var death := false
 var death_time := 1.5
 var moving := false
+var current_scene
+
+func _ready() -> void:
+	current_scene = get_tree().current_scene.scene_file_pathCurrent_level
+
 func  _process(delta: float) -> void:
-	if get_tree().current_scene.scene_file_path == "res://managers/Scenes/l_11.tscn" :
+	if current_scene == "res://managers/Scenes/l_11.tscn" :
 		moving = true
 		
 	if death == true:
